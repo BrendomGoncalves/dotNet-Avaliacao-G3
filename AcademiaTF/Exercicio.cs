a585083db08311105cec1a8cca2b486f0c00c3e0
Lucas Silva <lucas_avlis@outlook.com>
2023-12-03 14:24:32 -0300

    Adicionando tratamento de exeções no CRUD Exercício

--- AcademiaTF/Exercicio.cs
+++ AcademiaTF/Exercicio.cs
@@ -11,13 +11,13 @@ public class Exercicio
         Repeticoes = repeticoes;
         TempoIntervalo = tempoIntervalo;
     }
-    
+
     // Atributos
     private string? _grupoMuscular;
     private int _series;
     private int _repeticoes;
     private int _tempoIntervalo;
-    
+
     // Propriedades
     public string? GrupoMuscular
     {
@@ -64,19 +64,37 @@ public class Exercicio
         GrupoMuscular = Console.ReadLine();
 
         Console.Write("Quantidade de séries: ");
-        string? series = Console.ReadLine();
-      
-        Series = int.Parse(series);
+        string? seriesInput = Console.ReadLine();
+        int series;
+        while (!int.TryParse(seriesInput, out series))
+        {
+            Console.Write("Por favor, insira um número válido para a quantidade de séries: ");
+            seriesInput = Console.ReadLine();
+        }
+
+        Series = series;
 
         Console.Write("Quantidade de repetições: ");
-        string? repeticoes= Console.ReadLine();
-        
-        Repeticoes = int.Parse(repeticoes);
-        
+        string repeticoesInput = Console.ReadLine();
+        int repeticoes;
+        while (!int.TryParse(repeticoesInput, out repeticoes))
+        {
+            Console.Write("Por favor, insira um número válido para a quantidade de repetições: ");
+            repeticoesInput = Console.ReadLine();
+        }
+
+        Repeticoes = repeticoes;
+
 
         Console.Write("Tempo de intervalo (em segundos): ");
-        string? tempoIntervalo = Console.ReadLine();
-        TempoIntervalo = int.Parse(tempoIntervalo);
+        string tempoIntervaloInput = Console.ReadLine();
+        int tempoIntervalo;
+        while (!int.TryParse(tempoIntervaloInput, out tempoIntervalo))
+        {
+            Console.Write("Por favor, insira um número válido para o tempo de intervalo: ");
+            tempoIntervaloInput = Console.ReadLine();
+        }
+        TempoIntervalo = tempoIntervalo;
     }
 
     public void editarExercicio()
@@ -87,21 +105,21 @@ public class Exercicio
 
         Console.Write("Quantidade de séries: ");
         string? series = Console.ReadLine();
-      
+
         Series = int.Parse(series);
 
         Console.Write("Quantidade de repetições: ");
-        string? repeticoes= Console.ReadLine();
-        
+        string? repeticoes = Console.ReadLine();
+
         Repeticoes = int.Parse(repeticoes);
-        
+
 
         Console.Write("Tempo de intervalo (em segundos): ");
         string? tempoIntervalo = Console.ReadLine();
         TempoIntervalo = int.Parse(tempoIntervalo);
     }
 
-    
+
     public void imprimeExercicio()
     {
         Console.WriteLine($"{GrupoMuscular}\t\t  {Series}\t\t  {Repeticoes}\t\t\t{TempoIntervalo}");
