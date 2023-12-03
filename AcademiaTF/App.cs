5d9959492f6b6e4e99818bb307601f3f97744f91
leane <leanesoares212@gmail.com>
2023-12-02 23:30:06 -0300

    Relatorio aniversariantes

--- AcademiaTF/App.cs
+++ AcademiaTF/App.cs
@@ -13,6 +13,7 @@ public class App
             Console.WriteLine("1. Treinadores");
             Console.WriteLine("2. Clientes");
             Console.WriteLine("3. Exercícios");
+            Console.WriteLine("4. Treinadores e clientes aniversariantes");
             Console.WriteLine("0. Sair");
             Console.Write("> ");
             try
@@ -34,6 +35,34 @@ public class App
                     break;
                 case 3:
                     academia.menuExercicio();
+                    break;
+                case 4:
+                    Console.Clear();
+                    Console.WriteLine("Treinadores e clientes aniversariantes do mes");
+                    Console.Write("Digite o mes que deseja consultar (entre 1-12): ");
+                    int mesAniversario = int.Parse(Console.ReadLine() ?? "0");
+                    List<Treinador> treinadoresAniversariantes = _treinadores.Where(treinador =>
+                        treinador.Nascimento.Month == mesAniversario).ToList();
+                    List<Cliente> clientesAniversariantes = _clientes.Where(cliente =>
+                        cliente.Nascimento.Month == mesAniversario).ToList();
+                    Console.WriteLine("Treinadores aniversariantes:");
+                    if (treinadoresAniversariantes.Count > 0)
+                    {
+                        foreach (Treinador treinador in treinadoresAniversariantes)
+                        {
+                            treinador.imprimeTreinador();
+                        }
+                    }
+                    else Console.WriteLine("Nenhum treinador encontrado.");
+                    Console.WriteLine("Clientes aniversariantes:");
+                    if (clientesAniversariantes.Count > 0)
+                    {
+                        foreach (Cliente cliente in clientesAniversariantes)
+                        {
+                            treinador.imprimeCliente();
+                        }
+                    }
+                    else Console.WriteLine("Nenhum cliente encontrado.");
                     break;
                 case 0:
                     Console.Clear();
