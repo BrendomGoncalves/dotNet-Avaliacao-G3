fe75e860738c88763113e1452c799ee20457ca54
Beatriz-ux <beatrizpereira.ld@gmail.com>
2023-12-05 16:03:41 -0300

    feat: classe abstrata Pagamento

--- AcademiaTF/Pagamento.cs
+++ AcademiaTF/Pagamento.cs
@@ -1,6 +1,36 @@
 namespace AcademiaTF;
 
-public class Pagamento
+public abstract class Pagamento
 {
+    private String _descricao;
+    private double _valor;
+    private double _desconto;
+
+    private DateTime _dataHora;
+    public String Descricao { get=> _descricao; set{
+        if (value == null) throw new Exception("Descrição não pode ser nula");
+        if (value == "") throw new Exception("Descrição não pode ser vazia");
+        _descricao = value;
     
+    } }
+
+    public double ValorBruto { get=>_valor; set{
+        if (value < 0) throw new Exception("Valor não pode ser negativo");
+        if (value == 0) throw new Exception("Valor não pode ser zero");
+        _valor = value;
+    } }
+
+    public double Desconto { get=>_desconto; set{
+        if (value < 0) throw new Exception("Desconto não pode ser negativo");
+        if (value == 0) throw new Exception("Desconto não pode ser zero");
+        _desconto = value;
+
+    } }
+    public DateTime DataHora { get=>_dataHora; set{
+        if (value == DateTime.MinValue) throw new Exception("Data não pode ser vazia");
+        _dataHora = value;
+    } }
+
+
+}
 }
