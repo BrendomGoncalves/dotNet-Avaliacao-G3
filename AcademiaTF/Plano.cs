322a09620a8870a26d9e8c68c7cc0f4d7c54a49b
Myllena <myllenass.egq@gmail.com>
2023-12-05 15:27:55 -0300

    subindo classe plano

--- AcademiaTF/Plano.cs
+++ AcademiaTF/Plano.cs
@@ -2,5 +2,26 @@ namespace AcademiaTF;
 
 public class Plano
 {
-    
+    private string _titulo;
+    private double _valorPorMes;
+
+    public string? Titulo
+    {
+        get => _titulo;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value)) throw new Exception("Nome não pode ser um título nulo");
+            _titulo = value;
+        }
+    }
+    public double ValorPorMes
+    {
+        get => _valorPorMes;
+        set
+        {
+            if(double.IsNullOrWhiteSpace(value)) throw new Exception ("Não pode ser um valor nulo");
+            _valorPorMes = value
+        }
+    }
 }
+
